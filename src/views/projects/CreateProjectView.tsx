6fa7d1e53085f5b662866de339f9a35578e63730
Christian <chrisdaq@gmail.com>
2025-08-20 12:26:58 -0400

    add route for /projects/create and its view

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom"
 
-export const DashboardView = () => {
+export const CreateProjectView = () => {
 	return (
 		<>
-			<h1 className="text-5xl font-black">My projects</h1>
-			<p className="text-2xl font-light text-gray-500 mt-5">Manage your projects</p>
+			<h1 className="text-5xl font-black">Create Project</h1>
+			<p className="text-2xl font-light text-gray-500 mt-5">Fill the form to create a project</p>
 
 			<nav className="my-5">
 				<Link
 				className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-				to="/projects/create"
-			>New project</Link>
+				to="/"
+			>Return to projects</Link>
 			</nav>
 		</>
 	)
