8a5ad35e9ed0568f147450648d6bed777494681a
Christian <chrisdaq@gmail.com>
2025-08-21 15:51:12 -0400

    update vite alias config, working on createProject api call

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,11 @@
+import api from "@/lib/axios";
 import type { ProjectFormData } from "../types";
 
-export async function createProject(data: ProjectFormData) {
-	console.log(data);
+export async function createProject(formData: ProjectFormData) {
+	try {
+		const { data } = await api.post('/project', formData)
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
 }
