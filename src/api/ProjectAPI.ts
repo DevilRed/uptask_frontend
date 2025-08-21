ed93afed9f9ee6ac590852ba1868807006a1915a
Christian <chrisdaq@gmail.com>
2025-08-21 16:09:33 -0400

    fix wrong endpoint

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -3,7 +3,7 @@ import type { ProjectFormData } from "../types";
 
 export async function createProject(formData: ProjectFormData) {
 	try {
-		const { data } = await api.post('/project', formData)
+		const { data } = await api.post('/projects', formData)
 		console.log(data);
 	} catch (error) {
 		console.log(error);
