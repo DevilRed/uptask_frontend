95a750c4170cbda517fadc11533657c9b20dad10
Christian <chrisdaq@gmail.com>
2025-08-26 10:02:38 -0400

    add getProjectById api function, add edit project form, use it on edit project view

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,6 +1,23 @@
+import { getProjectById } from "@/api/ProjectAPI"
+import { useQuery } from "@tanstack/react-query"
+import { Navigate, useParams } from "react-router-dom"
+import { EditProjectForm } from "./EditProjectForm"
 
 export const EditProjectView = () => {
-	return (
-		<div>EditProjectView</div>
-	)
+	const params = useParams()
+	const projectId = params.projectId!
+
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ['editProject', projectId], // using dynamic params to avoid cache issues
+		queryFn: () => getProjectById(projectId), // using arrow fn to pass a parameter to the function
+		retry: false
+	})
+
+	if (isLoading) {
+		return 'Loading...'
+	}
+	if (isError) {
+		return <Navigate to='/404' />
+	}
+	if (data) return <EditProjectForm />
 }
