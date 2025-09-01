b2f72ab0a7731274d7fead12c0e32c5581cfa4cb
Christian <chrisdaq@gmail.com>
2025-09-01 11:45:19 -0400

    get task to edit from url params

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,9 +1,19 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getTaskById } from "@/api/TaskAPI"
 
 export const EditTaskData = () => {
+	const params = useParams()
+	const projectId = params.projectId!
 	const location = useLocation()
 	const queryParams = new URLSearchParams(location.search)
-	const taskId = queryParams.get('editTask')
+	const taskId = queryParams.get('editTask')!
+
+	const { data } = useQuery({
+		queryKey: ['task', taskId],
+		queryFn: () => getTaskById({ projectId: projectId, taskId: taskId })
+	})
+	console.log(data);
 	return (
 		<div>EditTaskData</div>
 	)
