ba009362c44b2ae4db1f44e80202fc8b07d0eb9c
Christian <chrisdaq@gmail.com>
2025-09-22 17:55:08 -0400

    add route to request code and API function to do so

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { ConfirmToken, UserRegistrationForm } from "../types";
+import type { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
 	try {
@@ -18,6 +18,19 @@ export async function createAccount(formData: UserRegistrationForm) {
 export async function confirmAccount(token: ConfirmToken) {
 	try {
 		const url = `/auth/confirm-account`
+		const { data } = await api.post<string>(url, token)
+		return data
+	} catch (error) {
+		if (isAxiosError(error) && error.response) {
+			throw new Error(error.response.data.error);
+
+		}
+	}
+}
+
+export async function requestConfirmationCode(token: RequestConfirmationCodeForm) {
+	try {
+		const url = `/auth/request-code`
 		const { data } = await api.post<string>(url, token)
 		return data
 	} catch (error) {
