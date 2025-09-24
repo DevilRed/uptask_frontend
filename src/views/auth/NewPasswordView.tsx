6dbcf49449b41a9845dbc7db32f39ac955edbcaf
Christian <chrisdaq@gmail.com>
2025-09-24 11:31:50 -0400

    add auth components to render them conditionally on NewPasswordView, add new NewPasswordForm type

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,6 +1,18 @@
+import { NewPasswordForm } from "@/components/auth/NewPasswordForm"
+import { NewPasswordToken } from "@/components/auth/NewPasswordToken"
+import { useState } from "react"
 
 export const NewPasswordView = () => {
+	const [isValidToken, setIsValidToken] = useState(false)
 	return (
-		<div>NewPasswordView</div>
+		<>
+			<h1 className="text-5xl font-black text-white">Password Reset</h1>
+			<p className="text-2xl font-light text-white mt-5">
+				Use the code you got  {''}
+				<span className=" text-fuchsia-500 font-bold"> on your email</span>
+			</p>
+
+			{!isValidToken ? <NewPasswordToken /> : <NewPasswordForm />}
+		</>
 	)
 }
