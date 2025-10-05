cb4a3ea507afbcad4dc2b93997c5d12e9dfb5a4c
Christian <chrisdaq@gmail.com>
2025-10-05 10:27:33 -0400

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,31 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  // ...tseslint.configs.strict,      // Enable for stricter linting.
+  // ...tseslint.configs.stylistic,   // Enable for code style rules.
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  prettier,
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["src/**/*.ts", "src/**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: { ecmaVersion: 2022, sourceType: "module" },
     },
+    plugins: {},
+    rules: {
+      // Custom TS rules
+      "@typescript-eslint/no-non-null-assertion": "off",
+    }
   },
-])
+  {
+    ignores: ["node_modules/**","build/**"],
+  },
+]
