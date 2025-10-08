281982c1bf071f7b0120efd38e0a6ddf67ebef8c
Christian <chrisdaq@gmail.com>
2025-10-08 17:54:54 -0400

    add endpoint to add user to project

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { Project, TeamMemberForm } from "../types";
+import type { Project, TeamMember, TeamMemberForm } from "../types";
 
 export async function findUserByEmail({
   projectId,
@@ -12,6 +12,24 @@ export async function findUserByEmail({
   try {
     const url = `/projects/${projectId}/team/find`;
     const { data } = await api.post(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function addUserToProject({
+  projectId,
+  id,
+}: {
+  projectId: Project["_id"];
+  id: TeamMember["_id"];
+}) {
+  try {
+    const url = `/projects/${projectId}/team`;
+    const { data } = await api.post(url, { id });
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
