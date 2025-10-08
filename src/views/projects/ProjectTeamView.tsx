4b5d999e902635d6ba837a96ea6dae5543e319cf
Christian <chrisdaq@gmail.com>
2025-10-08 10:23:43 -0400

    start working on project team view, include link to team on details view

--- src/views/projects/ProjectTeamView.tsx
+++ src/views/projects/ProjectTeamView.tsx
@@ -1,3 +1,29 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 export const ProjectTeamView = () => {
-  return <div>ProjectTeamView</div>;
+  const navigate = useNavigate();
+  const params = useParams();
+  const projectId = params.projectId;
+
+  return (
+    <>
+      <h1 className="text-5xl font-black">Manage Team</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Manage the team for this project</p>
+      <nav className="my-5 flex gap-3">
+        <button
+          type="button"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          onClick={() => navigate("?newMember=true")}
+        >
+          Add Colaborator
+        </button>
+        <Link
+          to={`/projects/${projectId}`}
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+        >
+          Return to project
+        </Link>
+      </nav>
+    </>
+  );
 };
