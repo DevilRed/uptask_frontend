3d2a06fd4ae201a0737a593b7eed79f6f6925604
Christian <chrisdaq@gmail.com>
2025-10-23 10:56:08 -0400

    check is current user owns the note, if so show delete button

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,19 +1,31 @@
+import { useAuth } from "@/hooks/useAuth";
 import type { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMemo } from "react";
 
 type NoteDetailProps = {
   note: Note;
 };
 
 export const NoteDetail = ({ note }: NoteDetailProps) => {
+  const { data, isLoading } = useAuth();
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data, note]);
+
+  if (isLoading) return "Loading...";
   return (
-    <div className="p-3 justify-between items-center">
+    <div className="p-3 flex justify-between items-center">
       <div>
         <p>
           {note.content} by: <span className="font-bold">{note.createdBy.name}</span>
         </p>
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+
+      {canDelete && (
+        <button className="bg-red-600 hover:bg-red-900 p-2 text-xs text-white font-bold cursor-pointer transition-colors">
+          Delete
+        </button>
+      )}
     </div>
   );
 };
