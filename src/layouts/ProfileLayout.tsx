1366eb561b0d7dccc470f2c2bf96fafad679eb02
Christian <chrisdaq@gmail.com>
2025-10-24 12:00:34 -0400

    add Tabs component for profile layout

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,8 +1,10 @@
+import Tabs from "@/components/profile/Tabs";
 import { Outlet } from "react-router-dom";
 
 export const ProfileLayout = () => {
   return (
     <>
+      <Tabs />
       <Outlet />
     </>
   );
